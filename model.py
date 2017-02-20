00c36792b844a0d6bccb486d446032d7e417a09a
eelzinaty <engessam1985@gmail.com>
2017-02-20 23:47:06 +0200

    fix model

--- model.py
+++ model.py
@@ -45,16 +45,22 @@ def get_comma_ai_model(shape):
 def get_nvidia_model(shape):
     model = Sequential()
     model.add(Cropping2D(cropping=((65, 20), (0, 0)), input_shape=(160, 320, 3)))
-    model.add(Lambda(lambda x: x/255 - 0.5, input_shape=shape))
+    model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=shape))
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(100))
+    model.add(Dropout(.2))
+    model.add(ELU())
+    model.add(Dense(50))
+    model.add(Dropout(.5))
+    model.add(ELU())
+    model.add(Dense(10))
+    model.add(Dropout(.5))
+    model.add(ELU())
     model.add(Dense(1))
 
     model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
@@ -101,7 +107,7 @@ if __name__ == "__main__":
         training_df, validation_df = load_training_validation_df(all_df)
         n = training_df.shape[0]
         batch_size = args.batch
-        samples_per_epoch = args.epochsize #int(n / batch_size)
+        samples_per_epoch = n * 3 # args.epochsize #int(n / batch_size)
 
         # Create training and validation generators
         train_gen = data_generator(training_df, batch_size=batch_size)
@@ -121,8 +127,8 @@ if __name__ == "__main__":
             os.makedirs(SAVE_HOME)
 
         # Create checkpoint at which model weights are to be saved
-        checkpoint = ModelCheckpoint(MODEL_HOME, monitor='val_loss', verbose=0,
-                                     save_best_only=True, save_weights_only=False, mode='auto')
+        checkpoint = ModelCheckpoint(MODEL_HOME)#, monitor='val_loss', verbose=0,
+                                     #save_best_only=True, save_weights_only=False, mode='auto')
 
         # Train the model
         model.fit_generator(train_gen, samples_per_epoch=samples_per_epoch, nb_epoch=args.epoch, callbacks=[checkpoint],
